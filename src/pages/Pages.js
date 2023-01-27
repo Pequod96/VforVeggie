267a45eda06e491fb1a5b85725bd877290260bbe
Samtoro <105719435+Pequod96@users.noreply.github.com>
2023-01-27 18:05:47 +0100

    second commit

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -7,7 +7,7 @@ import { Route, Routes } from 'react-router-dom';
 function Pages() {
   return (
      <Routes>
-       <Route path='/' element={<Home />} />
+       <Route path='/VforVeggie/' element={<Home />} />
        <Route path='/searched/:search' element={<Searched />} />
        <Route path='/recipe/:name' element={<Recipe />} />
      </Routes>
